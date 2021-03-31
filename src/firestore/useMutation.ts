ecdd3322e2cc2c7520c0fc17f2d306cd3a402580
ephremdeme <demelashephrem@gmail.com>
2021-03-31 16:53:04 +0300

    👌 IMPROVE: pass docRef to handle function

--- src/firestore/useMutation.ts
+++ src/firestore/useMutation.ts
@@ -2,28 +2,30 @@ import firebase from "firebase/app";
 
 import { useState } from "react";
 
-export const useDocumentAdd = <T>(
-  docRef: firebase.firestore.DocumentReference
-) => {
+export const useDocumentAdd = <T>() => {
   const [loading, setLoading] = useState<boolean>();
 
   const [error, setError] = useState<firebase.firestore.FirestoreError>();
 
-  const handleAdd = (data: Partial<T>) => {
+  const handleAdd = (
+    data: Partial<T>,
+    docRef: firebase.firestore.DocumentReference
+  ) => {
     setLoading(true);
     docRef.set(data).then(() => setLoading(false), setError);
   };
 
   return { loading, error, handleAdd };
 };
-export const useDocumentUpdate = <T>(
-  docRef: firebase.firestore.DocumentReference
-) => {
+export const useDocumentUpdate = <T>() => {
   const [loading, setLoading] = useState<boolean>();
 
   const [error, setError] = useState<firebase.firestore.FirestoreError>();
 
-  const handleUpdate = (data: Partial<T>) => {
+  const handleUpdate = (
+    data: Partial<T>,
+    docRef: firebase.firestore.DocumentReference
+  ) => {
     setLoading(true);
     docRef.update(data).then(() => setLoading(false), setError);
   };
